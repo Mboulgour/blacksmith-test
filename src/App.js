459de96258f770eed92f25d894eb8c357e0399f2
Mboulgour <mathieu.r.77@gmail.com>
2019-11-27 20:26:48 +0100

    user connection DONE + fetch album data DONE + album cover FIXED

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import hash from "./hash";
 
-function App() {
+import './App.scss';
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "08656aa303ef4b38a636260bf65b3be7";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+  
+const App = () =>{
+  const [token, setToken] = useState(null)
+  const [album, setAlbum] = useState([])
+  const [albumCover, setAlbumCover] = useState([])
+
+  useEffect(() =>{  
+    let _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchAlbumData = (token) => {
+      const url = "https://api.spotify.com/v1/albums/1lZahjeu4AhPkg9JARZr5F"
+      const config = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      };
+      // Make a call using the token
+      fetch(url, config)
+        .then(res => res.json())
+        .then(data => {
+          setAlbum(data)
+          setAlbumCover(data.images[1].url)
+        })
+    }
+    if(token){
+      fetchAlbumData(token)
+    }
+  }, [token])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token && (
+        <>
+          <h1>To continue, please :</h1>
+          <a className="connect__btn"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          >
+            Log in to Spotify
+          </a>
+          <a className="connect__info" href="#">Pourquoi dois-je me connecter ?</a>
+        </>
+      )}
+      {token && (
+        <div>
+          {album.name}
+          {console.log(album)}
+          {{albumCover} && (<img src={albumCover} alt={album.name} />)}
+        </div>
+      )}
     </div>
   );
 }
-
+  
 export default App;
+
