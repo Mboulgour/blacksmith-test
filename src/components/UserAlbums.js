8cda257362014b7059490eb08689904ef54952d0
Mboulgour <mathieu.r.77@gmail.com>
2019-12-02 14:23:56 +0100

    cleaning

--- src/components/UserAlbums.js
+++ src/components/UserAlbums.js
@@ -45,7 +45,6 @@ export const UserAlbums = () => {
               <Album {...album.album} />
             )}
           </div>
-          {console.log(userAlbums)}
         </div>
       )} 
       </div>  
